ee6a24625d0e92dce110f50d59460fa36ad98e2a
helloAbraham <abrahamemail@yahoo.com>
2021-01-29 20:35:18 -0500

    Just created

---
+++ apfs/src/main/java/com/apfs/ApfsApplication.java
@@ -0,0 +1,13 @@
+package com.apfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApfsApplication.class, args);
+	}
+
+}
---
+++ apfs/src/test/java/com/apfs/ApfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
