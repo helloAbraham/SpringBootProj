a09d9772b151558c5583725923485287a5a0560f
Mike Abi <info@mcsaccess.com>
2024-03-14 18:31:05 -0400

    added cross origin for test

--- apfs/src/main/java/com/apfs/Controller/ContactsController.java
+++ apfs/src/main/java/com/apfs/Controller/ContactsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200, https://mcsaccess.com")
 class ContactsController {
     private ContactRepository contactRepository;
 
